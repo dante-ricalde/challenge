95a020de229019b342a83510572f9271964efaca
Dante Ricalde <dantericalde@Dantes-MacBook-Pro-2.local>
2024-01-14 19:02:21 -0500

    add error controller advice + tests

--- backend-api/src/test/java/com/nearlinx/backendapi/expose/web/SimpleControllerTest.java
+++ backend-api/src/test/java/com/nearlinx/backendapi/expose/web/SimpleControllerTest.java
@@ -1,16 +1,15 @@
 package com.nearlinx.backendapi.expose.web;
 
+import org.hamcrest.Matchers;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -38,10 +37,8 @@ class SimpleControllerTest {
     void testEchoMessageNotProvided() throws Exception {
         mockMvc.perform(get("/api/echo"))
                 .andExpect(status().isBadRequest())
-                .andExpect(MockMvcResultMatchers
-                        .jsonPath("$.detail", Is.is("Required parameter 'message' is not present.")))
-                .andExpect(MockMvcResultMatchers
-                        .jsonPath("$.title", Is.is("Bad Request")));
+                .andExpect(jsonPath("$.detail", Is.is("Required parameter 'message' is not present.")))
+                .andExpect(jsonPath("$.title", Is.is("Bad Request")));
     }
 
     @Test
@@ -56,10 +53,8 @@ class SimpleControllerTest {
     void testSquareNumberNotProvided() throws Exception {
         mockMvc.perform(get("/api/square"))
                 .andExpect(status().isBadRequest())
-                .andExpect(MockMvcResultMatchers
-                        .jsonPath("$.detail", Is.is("Required parameter 'number' is not present.")))
-                .andExpect(MockMvcResultMatchers
-                        .jsonPath("$.title", Is.is("Bad Request")));
+                .andExpect(jsonPath("$.detail", Is.is("Required parameter 'number' is not present.")))
+                .andExpect(jsonPath("$.title", Is.is("Bad Request")));
     }
 
     @Test
@@ -67,9 +62,18 @@ class SimpleControllerTest {
         mockMvc.perform(get("/api/square")
                         .param("number", "abc"))
                 .andExpect(status().isBadRequest())
-                .andExpect(MockMvcResultMatchers
-                        .jsonPath("$.detail", Is.is("Parameter number must be a number.")))
-                .andExpect(MockMvcResultMatchers
-                        .jsonPath("$.title", Is.is("Bad Request")));
+                .andExpect(jsonPath("$.detail", Is.is("Parameter number must be a number.")))
+                .andExpect(jsonPath("$.title", Is.is("Bad Request")));
+    }
+
+    @Test
+    void testGetUsers() throws Exception {
+        mockMvc.perform(get("/api/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(4)))
+                .andExpect(jsonPath("$[0].name", Is.is("Admin")))
+                .andExpect(jsonPath("$[1].name", Is.is("User 1")))
+                .andExpect(jsonPath("$[2].name", Is.is("User 2")))
+                .andExpect(jsonPath("$[3].name", Is.is("User 3")));
     }
 }
